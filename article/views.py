7c58830655f9dce8844b5dc0ee10513307a2d8fb
jiqingfen110 <313466006@qq.com>
2018-06-05 20:40:41 +0800

    hao

--- article/views.py
+++ article/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.shortcuts import render_to_response,get_object_or_404
+from django.http import HttpResponse
 from .models import Article
 
 def article_detail(request,article_id):
-    try:
-        article = Article.objects.get(id = article_id)
-        context = dict()
-        context['article_obj'] = article
-        return render(request,'article_detail.html',context)
-    except Article.DoesNotExist:
-        raise Http404('cuowu!!')
+
+    article = get_object_or_404(Article,id =article_id)
+    context = dict()
+    context['article_obj'] = article
+    return render_to_response('article_detail.html',context)
+
+def article_list(request):
+    articles = Article.objects.filter(is_deleted= False)
+    context = dict()
+    context['articles'] = articles
+    return render_to_response('article_list.html', context)
 
 def index(request):
     return HttpResponse('hellos world')

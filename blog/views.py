fb57f607011326a41ef35c3f9e3fc663b776094d
jiqingfen110 <313466006@qq.com>
2018-06-07 09:22:23 +0800

    niu

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response,get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def blog_list(request):
+    context = dict()
+    context['blogs'] = Blog.objects.all()
+    return render_to_response('blog_list.html',context)
+
+def bolg_detail(request,blog_pk):
+    context = dict()
+    context['blog'] = get_object_or_404(Blog,id = blog_pk)
+    return
+

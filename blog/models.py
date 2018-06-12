2b8f1b6350051c9d45b125885d9c9656eeefcbc6
jiqingfen110 <313466006@qq.com>
2018-06-12 16:47:53 +0800

    2018.6.12

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+#定义后台文章类型
 class BlogType(models.Model):
     type_name = models.CharField(max_length = 15)
 
     def __str__(self):
         return self.type_name
-
+#定义后台admin博客数据库项
 class Blog(models.Model):
     title = models.CharField(max_length= 50)
     blog_type = models.ForeignKey(BlogType,on_delete=models.DO_NOTHING)

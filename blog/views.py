4266fc80eda85cb8efc787a8fc27f0996debb262
jiqingfen110 <313466006@qq.com>
2018-06-14 00:40:10 +0800

    201861401

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Blog,BlogType
 def blog_list(request):
     context = dict()
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html',context)
 #blog内容页views
 def bolg_detail(request,blog_pk):
@@ -19,4 +20,5 @@ def blogs_with_type(request,blog_type_pk):
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
     return render_to_response('blog/blogs_with_type.html',context)
+
 
